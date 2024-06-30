b444fc6612199b6d51321ecbb6f8872dbeb4e10b
Michele Liese <micheleliesesilva@gmail.com>
2024-06-30 14:32:05 -0300

    refactor: Improve game initialization logic and add random turn assignment

--- src/app.js
+++ src/app.js
@@ -14,20 +14,25 @@ let gameBoard = [];
 let gameStarted = false;
 
 const initializeGameBoard = () => {
-  const cards = [...Array(18)].map((_, index) => ({
+  const imageIds = [...Array(50)].map((_, index) => index);
+  for (let i = imageIds.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [imageIds[i], imageIds[j]] = [imageIds[j], imageIds[i]];
+  }
+  const selectedImageIds = imageIds.slice(0, 9);
+  const duplicatedImageIds = [...selectedImageIds, ...selectedImageIds];
+  gameBoard = duplicatedImageIds.map((imageId, index) => ({
     id: index,
-    image: index,
+    imageId: imageId,
     isFlipped: false,
     isMatched: false,
   }));
-  gameBoard = cards.sort(() => Math.random() - 0.5);
 };
 
 io.on("connection", (socket) => {
-  console.log("New player connected:", socket.id);
-
+  console.log(`New player connected: ${socket.id}`);
   socket.on("joinGame", (playerName) => {
-    console.log("Player", playerName, "trying to join the game");
+    console.log(`Player ${socket.id} joined the game with name: ${playerName}`);
     if (gameStarted) {
       socket.emit("gameJoined", {
         success: false,
@@ -36,10 +41,10 @@ io.on("connection", (socket) => {
     } else {
       players.push({ id: socket.id, name: playerName, score: 0, turn: false });
       if (players.length === 1) {
-        console.log("Player", socket.id, "is the host");
+        console.log(`Player ${socket.id} is the host`);
         socket.emit("host", socket.id);
       }
-      io.emit("playerJoined", players);
+      io.emit("players", players);
       socket.emit("gameJoined", {
         success: true,
         message: "You have successfully joined the game",
@@ -48,37 +53,65 @@ io.on("connection", (socket) => {
   });
 
   socket.on("startGame", () => {
-    gameStarted = true;
-    const randonIndex = Math.floor(Math.random() * players.length);
-    players = players.map((player, index) => ({
-      ...player,
-      turn: index === randonIndex,
-    }));
-    console.log("Game started with players:", players, "and random index:", randonIndex);
-    socket.emit("playerJoined", players);
-    initializeGameBoard();
-    io.emit("startedGame", gameBoard);
+    if (players.length < 2) {
+      console.log("Game cannot be started with less than 2 players");
+      socket.emit("gameStarted", {
+        success: false,
+        message: "Game cannot be started with less than 2 players",
+      });
+    } else {
+      gameStarted = true;
+      const randonIndex = Math.floor(Math.random() * players.length);
+      players = players.map((player, index) => ({
+        ...player,
+        turn: index === randonIndex,
+      }));
+      console.log(`Game started with players: ${players} and random index: ${randonIndex}`);
+      socket.emit("players", players);
+      initializeGameBoard();
+      io.emit("startedGame", gameBoard);
+    }
   });
 
   socket.on("flipCard", (cardIndex) => {
-    console.log("Flipped card:", cardIndex);
+    console.log(`Flipped card: ${cardIndex}`);
     io.emit("cardFlipped", { cardIndex, card: gameBoard[cardIndex] });
-    /**
-     * Implement a circular list algorithm to control players' turns
-     * Logic to check if flipped cards are a match
-     * Logic to check if flipped cards are different
-     * Logic to flip the cards back
-     * Logic to pass the turn to the other player
-     * Update players' scores
-     * Check if the game is over
-     * Send game over message
-     * Restart the game
-     *
-     */
+    if (gameBoard.filter((card) => !card.isMatched).length === 2) {
+      const flippedCards = gameBoard.filter((card) => card.isFlipped && !card.isMatched);
+      if (flippedCards[0].image === flippedCards[1].image) {
+        console.log(`Matched cards: ${flippedCards}`);
+        gameBoard = gameBoard.map((card) =>
+          flippedCards.some((flippedCard) => flippedCard.id === card.id)
+            ? { ...card, isMatched: true }
+            : card
+        );
+        players = players.map((player) =>
+          player.id === socket.id
+            ? { ...player, score: player.score + 1 }
+            : { ...player, turn: !player.turn }
+        );
+        io.emit("matchedCards", gameBoard);
+        io.emit("players", players);
+      } else {
+        console.log(`Flipped cards are different: ${flippedCards}`);
+        gameBoard = gameBoard.map((card) =>
+          flippedCards.some((flippedCard) => flippedCard.id === card.id)
+            ? { ...card, isFlipped: false }
+            : card
+        );
+        players = players.map((player) =>
+          player.id === socket.id
+            ? { ...player, turn: !player.turn }
+            : { ...player, turn: !player.turn }
+        );
+        io.emit("differentCards", gameBoard);
+        io.emit("players", players);
+      }
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("Player disconnected:", socket.id);
+    console.log(`Player disconnected: ${socket.id}`);
     players = players.filter((player) => player.id !== socket.id);
     io.emit("playerLeft", players);
     if (players.length < 2) {
@@ -88,4 +121,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(port, () => console.log(`Server running on http://${getIp().split(': ')[1]}:${port}`));
+server.listen(port, () =>
+  console.log(`Server running on http://${getIp().split(": ")[1]}:${port}`)
+);
